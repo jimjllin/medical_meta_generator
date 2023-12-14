290b43095fc950c94053741653f3acb467dd75cc
jimjllin <jim.jl.lin@gmail.com>
2023-12-14 13:58:46 +0800

    new

---
+++ convert_imaris_to_3dtexture.py
@@ -0,0 +1,42 @@
+import numpy as np
+import h5py
+import argparse
+
+def read_ims_file(filename):
+    """
+    從 .ims 檔案中讀取數據。
+
+    參數:
+    filename (str): .ims 檔案的路徑。
+
+    返回:
+    numpy.ndarray: 從檔案中讀取的數據。
+    """
+    with h5py.File(filename, 'r') as file:
+        # 根據檔案結構調整數據路徑
+        data_path = 'DataSet/ResolutionLevel 0/TimePoint 0/Channel 0/Data'
+        data = file[data_path][()]
+    return data
+
+def convert_to_raw(data, output_filename):
+    """
+    將 3D 數據轉換為 raw 格式並保存到文件。
+    """
+    data = data.astype(np.uint8)
+    data_bytes = data.tobytes()
+    with open(output_filename, 'wb') as file:
+        file.write(data_bytes)
+    print(f"文件 '{output_filename}' 已生成.")
+
+def main():
+    parser = argparse.ArgumentParser(description='將 Imaris .ims 檔案轉換為 Unity 的 3D texture raw 格式。')
+    parser.add_argument('--input', type=str, required=True, help='輸入的 .ims 檔案路徑')
+    parser.add_argument('--output', type=str, required=True, help='輸出的 raw 檔案路徑')
+
+    args = parser.parse_args()
+
+    data = read_ims_file(args.input)
+    convert_to_raw(data, args.output)
+
+if __name__ == "__main__":
+    main()
