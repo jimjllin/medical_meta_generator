5ae500139cdea59e1b4841ffab878645213c2673
jimjllin <jim.jl.lin@gmail.com>
2023-12-14 14:08:12 +0800

    print debug

--- convert_imaris_to_3dtexture.py
+++ convert_imaris_to_3dtexture.py
@@ -12,11 +12,18 @@ def read_ims_file(filename):
     返回:
     numpy.ndarray: 從檔案中讀取的數據。
     """
+    print("開始讀取 .ims 檔案...")
     with h5py.File(filename, 'r') as file:
-        # 根據檔案結構調整數據路徑
+        # 根據你的檔案結構調整這個路徑
         data_path = 'DataSet/ResolutionLevel 0/TimePoint 0/Channel 0/Data'
         data = file[data_path][()]
+
+    # 打印出 voxel 的尺寸
+    print(f"Voxel 尺寸: {data.shape}")
+
+    print("讀取完成。")
     return data
+
 
 def convert_to_raw(data, output_filename):
     """
