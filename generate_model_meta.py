192fc3e4fddc42b49de9f8c2f9c18e827ba48370
jimjllin <jim.jl.lin@gmail.com>
2023-12-27 09:41:45 +0800

    new

--- generate_model_meta.py
+++ generate_model_meta.py
@@ -30,58 +30,71 @@ def find_common_prefix(strings):
 def read_seg_ids_json(directory):
     seg_ids_file = join(directory, 'seg_ids.json')
     if not exists(seg_ids_file):
-        return []
+        return None
     with open(seg_ids_file, 'r') as file:
         return json.load(file)
 
 def generate_json(directory):
     seg_ids = read_seg_ids_json(directory)
     all_files = os.listdir(directory)
-    existing_files = {f for f in all_files if not f.startswith('.') and 
-                      (fnmatch.fnmatch(f, '*.ply') or fnmatch.fnmatch(f, '*.glb') or 
-                       fnmatch.fnmatch(f, '*.stl') or fnmatch.fnmatch(f, '*.fbx'))}
+    model_files = [f for f in all_files if not f.startswith('.') and 
+                   (fnmatch.fnmatch(f, '*.ply') or fnmatch.fnmatch(f, '*.glb') or 
+                    fnmatch.fnmatch(f, '*.stl') or fnmatch.fnmatch(f, '*.fbx'))]
     
     # 找出共同前綴
-    common_prefix = find_common_prefix(existing_files)
+    common_prefix = find_common_prefix(model_files)
 
-    distinct_colors = generate_distinct_colors(128)
+    distinct_colors = generate_distinct_colors(len(model_files))
     color_index = 0
 
     data = []
-    for item in seg_ids:
-        filename = item['filename']
-        if filename not in existing_files:
-            continue
+    if seg_ids:
+        for item in seg_ids:
+            filename = item['filename']
+            if filename not in model_files:
+                continue
 
-        name_without_extension = splitext(filename)[0]
-        cleaned_name = name_without_extension.replace(common_prefix, '', 1)  # 移除共同前綴
-        
-        # 若顏色用盡，則重新生成顏色列表
-        if color_index >= len(distinct_colors):
-            distinct_colors = generate_distinct_colors(128)
-            color_index = 0
+            name_without_extension = splitext(filename)[0]
+            cleaned_name = name_without_extension.replace(common_prefix, '', 1)
+            
+            color = distinct_colors[color_index]
+            color_index += 1
 
-        color = distinct_colors[color_index]
-        color_index += 1
-
-        segment = {
-            "label": cleaned_name,
-            "filename": filename,
-            "color": {
-                "r": color["r"],
-                "g": color["g"],
-                "b": color["b"],
-                "a": 0.7  # 將透明度調整為 0.7
+            segment = {
+                "label": cleaned_name,
+                "filename": filename,
+                "color": {
+                    "r": color["r"],
+                    "g": color["g"],
+                    "b": color["b"],
+                    "a": 0.7
+                }
             }
-        }
-        data.append(segment)
+            data.append(segment)
+    else:
+        for filename in model_files:
+            name_without_extension = splitext(filename)[0]
+            cleaned_name = name_without_extension.replace(common_prefix, '', 1)
+            
+            color = distinct_colors[color_index]
+            color_index += 1
+
+            segment = {
+                "label": cleaned_name,
+                "filename": filename,
+                "color": {
+                    "r": color["r"],
+                    "g": color["g"],
+                    "b": color["b"],
+                    "a": 0.7
+                }
+            }
+            data.append(segment)
 
     print(f"加入的模型檔案數量: {len(data)}")
-    print(f"seg_ids.json 中的物件數量: {len(seg_ids)}")
 
     output_file = join(directory, 'model_meta.json')
 
-    # 檢查輸出檔案是否已存在，如果存在則刪除
     if exists(output_file):
         os.remove(output_file)
     
